71f20ade4946951a04e499fbdde600402c429ce4
ghjkgf <zhaosenlin2018@gmail.com>
2023-01-12 22:25:13 +0800

    1

--- src/main/java/com/ethon/world/epic/EpicApplication.java
+++ src/main/java/com/ethon/world/epic/EpicApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EpicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EpicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EpicApplication.class, args);
+    }
 
 }
--- src/test/java/com/ethon/world/epic/EpicApplicationTests.java
+++ src/test/java/com/ethon/world/epic/EpicApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EpicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
