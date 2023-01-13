b422c4bc97f3f8bb129fc497a4cf9cc17f2a94ee
zsl <zhaosenlin@dbhs.com.cn>
2023-01-13 17:11:21 +0800

    4

---
+++ nginxStudy/src/main/java/com/example/nginxstudy/Test1.java
@@ -0,0 +1,9 @@
+package com.example.nginxstudy;
+
+/**
+ * @author zsl
+ * @date 2023/1/13
+ * @apiNote
+ */
+public class Test1 {
+}
---
+++ nginxStudy/src/test/java/com/example/nginxstudy/NginxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nginxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
