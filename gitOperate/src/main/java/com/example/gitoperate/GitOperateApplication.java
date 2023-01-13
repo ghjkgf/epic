28d692b6a72bc832fc94060b034a41c48baad000
zsl <zhaosenlin@dbhs.com.cn>
2023-01-13 16:20:24 +0800

    2

---
+++ gitOperate/src/main/java/com/example/gitoperate/GitOperateApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitoperate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitOperateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitOperateApplication.class, args);
+    }
+
+}
---
+++ gitOperate/src/test/java/com/example/gitoperate/GitOperateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitoperate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitOperateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
