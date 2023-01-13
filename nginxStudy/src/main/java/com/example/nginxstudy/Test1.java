f8d3ccd1915460b3895b66698b23844d83edea57
zsl <zhaosenlin@dbhs.com.cn>
2023-01-13 17:12:43 +0800

    7

--- nginxStudy/src/main/java/com/example/nginxstudy/Test1.java
+++ nginxStudy/src/main/java/com/example/nginxstudy/Test1.java
@@ -9,5 +9,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("2023");
         System.out.println("2022");
+        System.out.println("2028");
     }
 }
