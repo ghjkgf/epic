e6dc0eb1887e170a52f43b02f6788581af205b4d
zsl <zhaosenlin@dbhs.com.cn>
2023-01-15 17:02:25 +0800

    10

---
+++ streamStudy/src/test/java/com/example/streamstudy/StreamStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
