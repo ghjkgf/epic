6d56c4c8652d1ea992c544eaff5d0226b124eb58
zsl <zhaosenlin@dbhs.com.cn>
2023-01-19 11:50:15 +0800

    ddd

---
+++ JUC/Asynch/src/main/java/com/example/asynch/AsynchApplication.java
@@ -0,0 +1,13 @@
+package com.example.asynch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchApplication.class, args);
+    }
+
+}
